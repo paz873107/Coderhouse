9f817472327efd6e424cfef6d35008d0eb919c5e
paz873107 <60817728+paz873107@users.noreply.github.com>
2021-07-12 22:36:11 -0300

    Add files via upload

---
+++ Clases.ts
@@ -0,0 +1,2 @@
+export const suma = (numeroUno: number, numeroDos: number): number => numeroUno + numeroDos;
+export const resta = (numeroUno: number, numeroDos: number): number => numeroUno - numeroDos;
