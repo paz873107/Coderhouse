0946e6fc8c5fd63afc67f861f1ec1d5ad028e19d
paz873107 <paz873107@gmail.com>
2021-07-13 10:03:50 -0300

    first commit

---
+++ Tareas/Entregable 2/Clases.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.resta = exports.suma = void 0;
+var suma = function (numeroUno, numeroDos) { return numeroUno + numeroDos; };
+exports.suma = suma;
+var resta = function (numeroUno, numeroDos) { return numeroUno - numeroDos; };
+exports.resta = resta;
