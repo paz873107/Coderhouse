0e5fe0975677982595171f736afd297a3cf6e525
paz873107 <60817728+paz873107@users.noreply.github.com>
2021-07-12 22:36:44 -0300

    Delete Clases.ts

--- Clases.ts
+++
@@ -1,2 +0,0 @@
-export const suma = (numeroUno: number, numeroDos: number): number => numeroUno + numeroDos;
-export const resta = (numeroUno: number, numeroDos: number): number => numeroUno - numeroDos;
